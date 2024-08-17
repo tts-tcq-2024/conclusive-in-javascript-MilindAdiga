2728b42e1f3aec6500a4e0b3012fed38ee8f57af
MilindAdiga <124535083+MilindAdiga@users.noreply.github.com>
2024-08-17 19:03:49 +0530

    Update PassiveCoolingStrategy.js

--- src/cooling/PassiveCoolingStrategy.js
+++ src/cooling/PassiveCoolingStrategy.js
@@ -1,20 +1,8 @@
 const CoolingTypeStrategy = require('./CoolingTypeStrategy');
 
 class PassiveCoolingStrategy extends CoolingTypeStrategy {
-  classifyTemperature(temperatureInC) {
-    const lowerLimit = 0;
-    const upperLimit = 35;
-    return this.inferBreach(temperatureInC, lowerLimit, upperLimit);
-  }
-
-  inferBreach(value, lowerLimit, upperLimit) {
-    if (value < lowerLimit) {
-      return 'TOO_LOW';
-    }
-    if (value > upperLimit) {
-      return 'TOO_HIGH';
-    }
-    return 'NORMAL';
+  constructor() {
+    super(0, 35);  // Passing the specific limits to the parent class
   }
 }
 
